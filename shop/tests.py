d7436a6e47cfb7f569ac85f01b713c765d54c622
Enric <emieza@xtec.cat>
2024-10-04 01:41:12 +0200

    sim test

--- shop/tests.py
+++ shop/tests.py
@@ -2,5 +2,22 @@ from django.test import TestCase
 
 # Create your tests here.
 
-prova d'error
-
+"""ksdofkjsdf
+ds fsdkf
+d`spkfsd`fkds f`sdk `dsk f`ksd 
+dsf
+ sd
+ fks
+ d`fpk sd`fksd
+ fk sdfsd`fk`d fkdfs`gjk `j s
+ dgsjdo
+  gojg 
+  sdoj
+  sd ghs
+  dg h
+  sdg ih
+  sdihg 
+  psdhgsdg h
+  sd
+sdfdsjfdshfdsjf dsjf psdj fsdjfpsdijfpsdfjdsp fjsdpf jsdpf sdjfposd jfpds fpjodsfopjsdpfj sdpf
+"""
